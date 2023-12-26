0270696651d6e33a569a4ecf136d733e4b591597
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-12-26 16:24:14 +0900

    Update beginthreadexSample.cpp

--- SystemProgramming/Basic/Ch02/beginthreadexSample.cpp
+++ SystemProgramming/Basic/Ch02/beginthreadexSample.cpp
@@ -10,7 +10,11 @@ UINT WINAPI ThreadFunction(LPVOID pParam)
     return 0;
 }
 
-//C-RunTime-Library함수를 쓰게 되는 경우에 전역 변수나 정적데이터를 가지게 되는 경우
+//C-RunTime-Library가 제공하는 함수 중 정적 변수 혹은 전역 변수를 사용하는 함수는 멀티 스레드 환경에서 문제를 야기 할 수 있으므로
+//_beginthreadex()함수를 사용해라!
+//또는 TLS(Thread Local Storage)를 적용해서 문제를 해결해라!
+
+//C-RunTime-Library 제공하는 함수를 쓰게 되는 경우에 전역 변수나 정적데이터를 가지게 되는 경우
 //정적데이터를 VMS에서 처리하는 것이 생기게 되면, 스레드 간에 문제가 발생할 수 있다.
 //->해결책 : TLS(Thread Local Storage)
 //예)strtok함수같은 경우 문자열을 파싱을 하는 경우 T1 T2에서 호출하다가 충돌 날 수 있다. 
