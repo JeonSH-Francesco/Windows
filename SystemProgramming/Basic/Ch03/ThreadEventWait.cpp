a2bc974cfd2b180c6714555fc3d29b1e490ac866
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-12-28 16:18:24 +0900

    Update ThreadEventWait.cpp

--- SystemProgramming/Basic/Ch03/ThreadEventWait.cpp
+++ SystemProgramming/Basic/Ch03/ThreadEventWait.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+using namespace std;
 #include <conio.h>
 #include <process.h>
 #include <windows.h>
 
-UINT WINAPI ThreadFunction(LPVOID pParam)
-{
+UINT WINAPI ThreadFunction(LPVOID pParam) {
     HANDLE hEvent = pParam;
-    std::cout << "ThreadFunction() - Begin" << std::endl;
+    cout << "ThreadFunction() - Begin" << endl;
 
     _getch();
     SetEvent(hEvent);
-    std::cout << "ThreadFunction() - SetEvent()" << std::endl;
-    std::cout << "ThreadFunction() - End" << std::endl;
+    cout << "ThreadFunction() - SetEvent()" << endl;
+    cout << "ThreadFunction() - End" << endl;
     return 0;
 }
 
+
 int main()
 {
-    std::cout << "main() - Begin" << std::endl;
+    cout << "main() - Begin" << endl;
     HANDLE hEvent = ::CreateEvent(NULL, FALSE, FALSE, NULL);
 
     UINT nThreadId = 0;
@@ -32,16 +33,45 @@ int main()
         std::cout << "ERROR: Failed to begin thread." << std::endl;
         return 0;
     }
-    
+
     if (::WaitForSingleObject(
         hEvent, INFINITE) == WAIT_OBJECT_0)
     {
-        std::cout << "main() - WaitForSingleObject() return" << std::endl;
+        cout << "main() - WaitForSingleObject() return" << endl;
     }
 
     ::WaitForSingleObject(hThread, INFINITE);
-    std::cout << "main() - Thread End" << std::endl;
+    cout << "main() - Thread End" << endl;
+
     ::CloseHandle(hThread);
-    std::cout << "main() - End" << std::endl;
+    cout << "main() - End" << endl;
     return 0;
 }
+
+/*
+HANDLE은 POINTER다.
+WaitForSingleObject 함수는 가리키고 있는 대상(핸들)이 들어가는데, 이벤트의 변화를 감지하는 함수다.(이벤트가 Set 되는 시점을 감지한다.)
+의도적으로 시점을 만들어낼 때, Event를 사용한다.
+Event 상태는 Set Reset 두가지가 있다.
+->시점 동기화 : Event
+
+결과 출력 경우 2가지
+-------------------------------------
+main() - Begin
+ThreadFunction() - Begin
+ThreadFunction() - SetEvent()
+ThreadFunction() - End
+main() - WaitForSingleObject() return
+main() - Thread End
+main() - End
+
+--------------------------------------
+main() - Begin
+ThreadFunction() - Begin
+ThreadFunction() - SetEvent()main() - WaitForSingleObject() return
+
+ThreadFunction() - End
+main() - Thread End
+main() - End
+
+*/
