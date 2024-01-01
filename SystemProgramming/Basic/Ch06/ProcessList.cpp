846a744d2b9fe4a91cdf48b34ac255d02b9ca817
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2024-01-01 17:54:57 +0900

    Create ProcessList.cpp

---
+++ SystemProgramming/Basic/Ch06/ProcessList.cpp
@@ -0,0 +1,51 @@
+// ProcessList01.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <tchar.h>
+#include <windows.h>
+#include <psapi.h>
+
+int main()
+{
+	_wsetlocale(LC_ALL, L"korean");
+
+	DWORD		pidArray[512] = { 0 };
+	DWORD		cbNeeded = 0;
+	DWORD		nProcesses = 0;
+	TCHAR		szBuffer[MAX_PATH + _MAX_FNAME] = { 0 };
+	UINT		uCounter = 0;
+
+
+	if (::EnumProcesses(pidArray, sizeof(pidArray), &cbNeeded))
+	{
+		nProcesses = cbNeeded / sizeof(DWORD);
+		for (DWORD i = 0; i <= nProcesses; i++)
+		{
+			HANDLE hProcess;
+			DWORD pid = pidArray[i];
+      
+			//SYSTEM Permission 함수인 경우는 권한이 거부됨.(ERROR CODE : 5)
+			hProcess = ::OpenProcess(
+				PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
+			if (hProcess != NULL)
+			{
+				::ZeroMemory(szBuffer, MAX_PATH + _MAX_FNAME);
+				if (::GetModuleFileNameEx(hProcess, NULL,
+					szBuffer, MAX_PATH + _MAX_FNAME) != NULL)
+				{
+					_tprintf(TEXT("[PID: %d] %s\n"), pid, szBuffer);
+				}
+				::CloseHandle(hProcess);
+			}
+			else
+			{
+				_tprintf(TEXT("[PID: %d, Error code: %d]\n"),
+					pid, ::GetLastError());
+				continue;
+			}
+		}
+	}
+
+	return 0;
+}
