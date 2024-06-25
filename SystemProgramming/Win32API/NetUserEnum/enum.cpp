786fb1cfd80b8f6a5671a02961ec1c420be61f89
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2024-06-25 22:40:21 +0900

    Update enum.cpp

--- SystemProgramming/Win32API/NetUserEnum/enum.cpp
+++ SystemProgramming/Win32API/NetUserEnum/enum.cpp
@@ -1,19 +1,20 @@
 #include <Windows.h>
-#include <lmaccess.h>
-#include <lmapibuf.h>
+#include <LMaccess.h>
+#include <LMAPIbuf.h>
 #include <stdio.h>
 #include <locale.h>
 #pragma comment(lib,"netapi32")
 
-int main()
-{
-    setlocale(LC_ALL, "KOREAN");
+int main() {
+	setlocale(LC_ALL, "KOREAN");
 
-    PUSER_INFO_0 buffer;
-    DWORD rcnt = 0;
-    DWORD total = 0;
-    
-    NetUserEnum(NULL,//서버 이름(NULL이면 로컬 컴퓨터)
+	PUSER_INFO_0 buffer;
+	DWORD rcnt = 0;
+	DWORD total = 0;
+
+	NetUserEnum(NULL, 0, 0, (LPBYTE*)&buffer, 0xFFFFFFFF, &rcnt, &total, NULL);
+	/*차례대로 
+	    NetUserEnum(NULL,//서버 이름(NULL이면 로컬 컴퓨터)
         0, //데이터 정보 수준 0일 때 USER_INFO_0 구조체 배열
         0, //계정유형 (0이면 모든 일반 사용자, 트로스트 데이터 및 컴퓨터 계정 데이터 포함)
         (LPBYTE*)&buffer,//수신할 버퍼의 포인터(시스템에 의해 할당)
@@ -21,8 +22,7 @@ int main()
         &rcnt,//실제 열거한 요소 수
         &total,//다시 시작에서 열거할 수 있는 총 항목 수(힌트로만 고려)
         NULL);//계속 검색할 때 사용하는 핸들
-    
-    
+	*/
     for (DWORD i = 0; i < total; i++)
     {
         wprintf(TEXT("%s\n"), buffer[i].usri0_name);
